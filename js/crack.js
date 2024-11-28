981a61451ad485ca44b8ccc8e069fa6b015f86a5
samaramorgans <s00927963@gmail.com>
2024-11-28 22:28:00 +0300

    Update crack.js

--- js/crack.js
+++ js/crack.js
@@ -3,6 +3,3 @@ localStorage.setItem('user_login', 'halabja88');
 localStorage.setItem('user_password', 'halabja88');
 localStorage.setItem('$hlsplayer$', '3');
 localStorage.setItem('$devices$', '{"halajba88":["48024000623e46ae9c9a73b84535f19a7845b0b5","unknown"]}');
-// Сохранение объекта (необходимо преобразовать в строку)
-const user = { name: 'John', age: 30 };
-localStorage.setItem('user', JSON.stringify(user));

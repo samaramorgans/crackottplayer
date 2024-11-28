cef3386c1a2269a05d297834b6d96c0aef51206a
samaramorgans <s00927963@gmail.com>
2024-11-28 21:53:30 +0300

    Add files via upload

---
+++ js/crack.js
@@ -0,0 +1,7 @@
+
+localStorage.setItem('user_login', 'halabja88');
+localStorage.setItem('user_password', 'halabja88');
+localStorage.setItem('$devices$', '{"halabja88":["ca2bbb73e721fc6b69b83bdfbe521c4039403383","ACTIVE - تفعيل - چالاك كردن"]}');
+// Сохранение объекта (необходимо преобразовать в строку)
+const user = { name: 'John', age: 30 };
+localStorage.setItem('user', JSON.stringify(user));
